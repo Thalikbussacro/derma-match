e70562dc5136fb4dd2adc3b11bd625d1250b4598
Thalik Bussacro <50772851+Thalikbussacro@users.noreply.github.com>
2026-07-02 22:02:38 -0300

    adiciona eslint flat config e prettier com scripts de lint e format

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**'],
+  },
+  {
+    files: ['**/*.ts'],
+    extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'error',
+    },
+  },
+  eslintConfigPrettier,
+);
